6ca5751bcabcf79718cf62a305fc64e3491032e6
AmandineGervis <gervis.amandine@gmail.com>
2020-11-19 16:31:37 +0100

    aubarges

--- hackathon1/src/components/Auberges.js
+++ hackathon1/src/components/Auberges.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { auberges } from "./dataAuberges";
+import React from 'react';
+import { auberges } from './dataAuberges';
 
 const Auberges = () => {
   return (
     <div>
       {auberges.map((auberge) => {
         return (
-          <div key={auberge.id}>
-            <img src={auberge.image} alt={auberge.name} />
-            <h4>{auberge.name}</h4>
-            <p>{auberge.description}</p>
-            <p>{`Delivry Price : ${auberge.delivryPrice}`}</p>
-            <p>{auberge.duration}</p>
+          <div key={auberge.id} className='container'>
+            <img src={auberge.image} alt={auberge.name} id='imageAuberge' />
+            <h4 className='name'>{auberge.name}</h4>
+            <p className='location'>{auberge.location}</p>
+            <p className='delivery'>{`Delivery Price : ${auberge.delivryPrice}`}</p>
+            <p className='duration'>{auberge.duration}</p>
           </div>
         );
       })}
@@ -20,27 +20,3 @@ const Auberges = () => {
 };
 
 export default Auberges;
-/*     {/*  <h1>Auberges</h1>
-      <div className="Auberge">
-        <img src={props.image} alt={props.name} />
-        <h4>{props.name}</h4>
-        <p>{props.description}</p>
-        <p>{`Delivry Price : ${props.delivryPrice}`}</p>
-        <p>{props.duration}</p>
-      </div> */
-
-/* 
-import React from "react";
-import { movies } from "../data";
-import Movie from "./Movie";
-
-const MovieList = () => (
-  <div>
-    {movies.map((movie) => (
-      <Movie key={movie.id} data={movie} />
-    ))}
-  </div>
-);
-
-export default MovieList;
- */
