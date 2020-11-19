e6a0b775f9a295560d08f8d9d1c366395fd09ba8
Aymeric <aymeric.bouault@gmail.com>
2020-11-19 22:54:35 +0100

    creation nouvelle branche

--- hackathon1/src/App.js
+++ hackathon1/src/App.js
@@ -9,17 +9,19 @@ import Rechercher from './components/Rechercher';
 import Commandes from './components/Commandes';
 import Compte from './components/Compte';
 import Accueil from './components/Accueil';
+import Confirmation from './components/Confirmation';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar />    
         <Switch>
           <Route exact path="/" component={Accueil}/>
           <Route exact path= "/rechercher" component={Rechercher}/>
           <Route exact path= "/commandes" component={Commandes}/>
           <Route exact path= "/compte" component={Compte}/>
+          <Route exact path="/confirmation" component={Confirmation} />
         </Switch>
       </Router>
     </div>
---
+++ hackathon1/src/components/Confirmation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Confirmation = () => {
+    return(
+        <div>
+            123
+        </div>
+    )
+}
+
+export default Confirmation;
