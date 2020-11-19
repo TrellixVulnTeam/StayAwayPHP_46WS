5d8885002dddfcf3b0d3aea8b605011d53a5b86d
Aymeric <aymeric.bouault@gmail.com>
2020-11-20 00:16:33 +0100

    css page accueil

--- hackathon1/src/components/Accueil.js
+++ hackathon1/src/components/Accueil.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/Accueil.css';
+/* import logo from '../images/logo.png'; 
+import gif from '../images/chevalier.gif';  */
 
 const Accueil = () => {
-    return(
-        <div>
-           
-        </div>
-    )
-}
+  return (
+    <div className='container-home-page'>
+      <img className='logo' src='../images/logo.png' alt='logo' />
+      <div className='container-btn-home'>
+        <Link to='/weapon'>
+          <button type='button' className='btn1'>
+            EXPRESS WEAPONS
+          </button>
+        </Link>
+        <Link to='/food'>
+          <button type='button' className='btn2'>
+            EXPRESS MEALS
+          </button>
+        </Link>
+        <img className='gif' src='../images/chevalier.gif' alt='logo' />
+      </div>
+    </div>
+  );
+};
 
 export default Accueil;
