5c492b2679bcf08fee9fb4a05e6a9f941cc7e2ef
cleroy117 <ce.leroy69@gmail.com>
2020-11-19 18:56:39 +0100

    pictures works yeahhhh

--- hackathon1/src/components/Meals.js
+++ hackathon1/src/components/Meals.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Meals = () => {
   const [meals, setMeals] = useState([]);
@@ -9,19 +9,26 @@ const Meals = () => {
 
   const getMeals = () => {
     axios
-      .get('https://www.themealdb.com/api/json/v1/1/random.php')
+      .get("https://www.themealdb.com/api/json/v1/1/random.php")
       // Extract the DATA from the received response
-      .then((response) => response.data)
+      .then((response) => response.data.meals)
       // Use this data to update the state
       .then((data) => {
-        console.log(data);
         setMeals(data);
       });
   };
+  console.log(meals);
 
   return (
     <div>
-      <img src={meals.strMealThumb} alt='' />
+      {meals.map((meal) => (
+        <div>
+          <img src={meal.strMealThumb} alt={meal.strIngredient1} />
+          <p>{meal.strMeal}</p>
+          <p>{`${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, ${meal.strIngredient4}`}</p>
+          <button type="button">Add festoylle</button>
+        </div>
+      ))}
     </div>
   );
 };
