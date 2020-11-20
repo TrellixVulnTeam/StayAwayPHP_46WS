6350d83e61390d2900e707867a83c2410877c565
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-11-20 04:38:53 +0100

    modified some css

--- hackathon1/src/components/CarouselFood.js
+++ hackathon1/src/components/CarouselFood.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../style/CarouselFood.scss';
 import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const slides = [
   {
--- hackathon1/src/components/Navbar.js
+++ hackathon1/src/components/Navbar.js
@@ -1,31 +1,28 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/Navbar.css';
 
 const Navbar = () => {
-    return(
-        <div>
-            <nav >
-                <ul className="menu">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/food">Food</Link>
-                    </li>
-                    <li>
-                        <Link to="/carouselweapon">Weapon</Link>
-                    </li>
-                    
-                    <li>
-                        <Link to="/commandes">Orders</Link>
-                    </li>
-                    
-                    
-                </ul>
-            </nav>
-        </div>
-    )
-}
+  return (
+    <div>
+      <nav>
+        <ul className='menu'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/food'>Food</Link>
+          </li>
+          <li>
+            <Link to='/carouselweapon'>Weapon</Link>
+          </li>
+          <li>
+            <Link to='/commandes'>Orders</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
